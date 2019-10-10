145a070a2662a0a3cedd08069cfb37c5925b2360
Vardhman Golchha <vardhmangolchha@Vardhmans-MacBook-Pro.local>
2019-10-10 07:25:55 +0400

    changes

--- src/main/java/com/assign/utilities/ExtentManager.java
+++ src/main/java/com/assign/utilities/ExtentManager.java
@@ -2,30 +2,39 @@ package com.assign.utilities;
 
 import java.io.File;
 
+import com.assign.base.Constants;
 import com.relevantcodes.extentreports.DisplayOrder;
 import com.relevantcodes.extentreports.ExtentReports;
 
 public class ExtentManager {
-	
-	private static ExtentReports extent;
-	
-	
-	public static ExtentReports getInstance(){
-		
-		if(extent==null){
-			
-			
-			extent = new ExtentReports(System.getProperty("user.dir")+"/target/surefire-reports/html/extent.html",true,DisplayOrder.OLDEST_FIRST);
-			extent.loadConfig(new File(System.getProperty("user.dir")+"/src/test/resources/extentconfig/ReportsConfig.xml"));
-			extent.addSystemInfo("Browser Name", "Chrome");
-	        extent.addSystemInfo("Browser version", "v78.0");
-	        extent.addSystemInfo("Selenium version", "v3.6.0");
 
+	private static ExtentReports extent;
+
+	public static ExtentReports getInstance() {
+
+		if (extent == null) {
+
+			extent = new ExtentReports(System.getProperty("user.dir") + "/target/surefire-reports/html/extent.html",
+					true, DisplayOrder.OLDEST_FIRST);
+			extent.loadConfig(
+					new File(System.getProperty("user.dir") + "/src/test/resources/extentconfig/ReportsConfig.xml"));
+			if(Constants.browser.equals("chrome")) {
+				extent.addSystemInfo("Browser Name", "Chrome");
+				extent.addSystemInfo("Browser version", "v78.0");
+				extent.addSystemInfo("Selenium version", "v3.6.0");
+			}
+			else {
+				extent.addSystemInfo("Browser Name", "Firefox");
+				extent.addSystemInfo("Browser version", "v69.0.2");
+				extent.addSystemInfo("Selenium version", "v3.6.0");
+				
+			}
 			
+
 		}
-		
+
 		return extent;
-		
+
 	}
 
 }
--- src/test/java/com/assign/testcases/GoogleSearchTest.java
+++ src/test/java/com/assign/testcases/GoogleSearchTest.java
@@ -1,64 +1,58 @@
 package com.assign.testcases;
-import org.testng.annotations.AfterMethod;
+
+import org.apache.log4j.Logger;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.assign.base.Page;
-import com.assign.errorcollectors.ErrorCollector;
 import com.assign.pages.actions.GoogleSearchPage;
-import com.assign.utilities.Log;
-import com.assign.utilities.Utilities;
-import org.apache.log4j.Logger;
 
 public class GoogleSearchTest {
-	
+
 	public static Logger log = Logger.getLogger("devpinoyLogger");
-	//intializing the driver and Page Factory Objects
+
+	// intializing the driver and Page Factory Objects
 	@BeforeSuite
-	public void setUp(){
-		
+	public void setUp() {
+
 		Page.initConfiguration();
 	}
-	
+
 	/**
-	 *  Test Case
-	 * @throws InterruptedException
-	 * Test Steps : 1. Open https://www.google.com/
-	 * 2. Search for “programming”
-	 * 3. Parse result page and print title 
-	 * and sections (sections are various result hits from different web sites which are listed by google on the results page 
-	 * and the titles are titles of those section please skip the google ads and maps part if there is such a result on the result page).
-	 * 4. Go to next page and do the same
+	 * Test Case
+	 * 
+	 * @throws InterruptedException Test Steps : 1. Open https://www.google.com/ 2.
+	 *                              Search for “programming” 3. Parse result page
+	 *                              and print title and sections (sections are
+	 *                              various result hits from different web sites
+	 *                              which are listed by google on the results page
+	 *                              and the titles are titles of those section
+	 *                              please skip the google ads and maps part if
+	 *                              there is such a result on the result page). 4.
+	 *                              Go to next page and do the same
 	 */
 	@Test
 	public void printTitleAndSections() throws InterruptedException {
-		
+
 		log.debug("Starting test case printTitleAndSections");
-		String keywordToSearch ="Programming";
-		String page2StatsId="resultStats";
-		GoogleSearchPage gp= new GoogleSearchPage();
+		String keywordToSearch = "Programming";
+		String page2StatsId = "resultStats";
+		GoogleSearchPage gp = new GoogleSearchPage();
 		gp.searchOnGoogle(keywordToSearch);
 		gp.printLinkTextAndSectionsToFile();
 		gp.clickOnPageNumber();
 		gp.wait(page2StatsId);
-		gp.printLinkTextAndSectionsToFile(); 
-		log.debug("Test case printTitleAndSections completed successfully" );
-		
-		
+		gp.printLinkTextAndSectionsToFile();
+		log.debug("Test case printTitleAndSections completed successfully");
+
 	}
-	
-	@AfterSuite(alwaysRun=true)
-	public static void quitBrowser(){
-		
+
+	@AfterSuite(alwaysRun = true)
+	public static void quitBrowser() {
+
 		Page.tearDown();
-		
+
 	}
-	
-	
-	
 
 }
